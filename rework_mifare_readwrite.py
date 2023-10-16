0c7e05af9cbad63cece3bbb098ffc5d6d02c6d43
jimsio <klemens.frick@gmail.com>
2023-10-16 12:07:20 +0200

    mifare readwrite

--- rework_mifare_readwrite.py
+++ rework_mifare_readwrite.py
@@ -49,19 +49,45 @@ while True:
 print("")
 
 print("Found card with UID:", [hex(i) for i in uid])
-print("Authenticating block 4 ...")
 
+#mifare 1K layout (chip + card)
+# 1 kByte
+
+# 16 Sektoren zu je 4 Blöcken (16 Bytes/16 Ascii Characters pro Block)
+
+#writeable blocks (https://support.ccs.com.ph/portal/en/kb/articles/mifare-classic-1k-memory-structure)
+# 4, 5, 6
+# 8, 9, 0A,
+# 0C, 0D, 0E,...
+
+#allow only 16 ascii characters, so i only need one block, block 4
+# 2 characters for prefix "en", 1 for suffix "#", so my word can have 13 characters!
+
+print("Authenticating block 4 ...")
 authenticated = pn532.mifare_classic_authenticate_block(uid, 4, MIFARE_CMD_AUTH_B, key)
 if not authenticated:
     print("Authentication failed!")
 
-# Set 16 bytes of block to 0xFEEDBEEF
-data = bytearray(16)
-data[0:16] = b"\xFE\xED\xBE\xEF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
-# Write 16 byte block.
+data = bytearray(16)
+
+lang = "en"
+message = "MANFREd" #can be 13 characters long
+endofmessage = "#"
+message = lang+message+endofmessage
+
+data[0:len(message)] = message.encode()
+
+print(data)
+
+# Set 16 bytes of block to 0xFEEDBEEF
+#data = bytearray(16)
+#data[0:16] = b"\xFE\xED\xBE\xEF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
+# Write 16 byte to block 4.
 pn532.mifare_classic_write_block(4, data)
-# Read block #6
+
+# Read block
 print(
     "Wrote to block 4, now trying to read that data:",
     [hex(x) for x in pn532.mifare_classic_read_block(4)],
